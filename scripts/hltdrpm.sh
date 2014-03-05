75d59ebd4a74971868e93013d3349868dcbe8fd9
Srecko Morovic <srecko.morovic@cern.ch>
2014-03-05 11:01:34 +0100

    bump version to 1.2.3

--- scripts/hltdrpm.sh
+++ scripts/hltdrpm.sh
@@ -181,7 +181,7 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > hltd.spec <<EOF
 Name: hltd
-Version: 1.2.2
+Version: 1.2.3
 Release: 0
 Summary: hlt daemon
 License: gpl
