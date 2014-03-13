931c5c10dabc4955140c8135d625732e99d005a3
Srecko Morovic <srecko.morovic@cern.ch>
2014-03-13 13:37:55 +0100

    rpm version 1.2.93 for this branch

--- scripts/hltdrpm.sh
+++ scripts/hltdrpm.sh
@@ -217,7 +217,7 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > hltd.spec <<EOF
 Name: hltd
-Version: 1.2.92
+Version: 1.2.93
 Release: 0
 Summary: hlt daemon
 License: gpl
