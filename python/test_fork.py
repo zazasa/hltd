2c6dfb6fcaa575198a781be15d79928e83322b9c
Srecko Morovic <srecko.morovic@cern.ch>
2014-04-04 17:19:41 +0200

    delete old unused files

--- python/test_fork.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python2.6
-
-import hltdconf
-import demote
-import subprocess
-conf=hltdconf.hltdConf('/etc/hltd.conf')
-new_run_args = ['watch','-n 1', 'ls']
-dem = demote.demote(conf.user)
-subprocess.Popen(new_run_args,preexec_fn=dem,close_fds=True)
