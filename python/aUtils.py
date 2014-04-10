17205001691b5f6374e4d7416fcc8eca03bdb6ca
zazasa <salvatore.zaza@gmail.com>
2014-04-10 15:56:52 +0200

    see previous commit

--- python/aUtils.py
+++ python/aUtils.py
@@ -243,7 +243,7 @@ class fileHandler(object):
         return True
 
     def moveFile(self,newpath,copy = False):
-        if not self.exists: return True
+        if not self.exists(): return True
         oldpath = self.filepath
         newdir = os.path.dirname(newpath)
 
@@ -280,7 +280,7 @@ class fileHandler(object):
         return True
 
     def esCopy(self):
-        if not self.exists: return
+        if not self.exists(): return
         if self.filetype in TO_ELASTICIZE:
             esDir = os.path.join(self.dir,ES_DIR_NAME)
             if os.path.isdir(esDir):
