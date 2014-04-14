678dd829118fb8b5fae5285d7162cee038abeab8
Srecko Morovic <srecko.morovic@cern.ch>
2014-04-14 16:48:45 +0200

    avoid error in inotify exception in stopping (it is part of interrupting inotify read)

--- python/inotifywrapper.py
+++ python/inotifywrapper.py
@@ -76,7 +76,7 @@ class InotifyWrapper(threading.Thread):
                       #parent does not implement the function
                       self.parent_.process_default(event)
           except Exception, ex:
-              self.logger.exception(ex)
+              if self.quit == False: self.logger.error("exception in inotify run thread: "+ str(ex))
 
     def stop(self):
         self.quit = True
